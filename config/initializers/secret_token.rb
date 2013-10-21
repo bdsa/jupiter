f58c4aba9343c12649387d16359f12b205579be7
bdsa <bdsa@server.fake>
2013-10-21 17:25:11 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jupiter::Application.config.secret_token = 'f932fd08a0cbd472926b242e343f909bbb9f3168ea0265299ded7ac36dfdc70d5d92b3bb5e952cb002a7e53c43a18ccd1e1caa13f53d966905cf3eb6ec266f53'
